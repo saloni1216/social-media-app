54b87da159e1fa03ce456903d5f79571b503c784
Saloni singh <131551585+saloni1216@users.noreply.github.com>
2026-07-15 23:34:48 +0530

    update

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -132,10 +132,10 @@ USE_TZ = True
 
 
 
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS = [BASE_DIR / "static",]
-# STATIC_ROOT = BASE_DIR / "staticfiles"
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static",]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 STORAGES = {
