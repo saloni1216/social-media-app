c7632f40eae414e208c9bd64f10740041d8fd70f
Saloni singh <131551585+saloni1216@users.noreply.github.com>
2026-07-15 21:51:01 +0530

    imagehelper.js

--- frontend/src/utils/imageHelper.js
+++ frontend/src/utils/imageHelper.js
@@ -1,18 +1,19 @@
-export const getImageUrl = (image) => {
+const BASE_URL = import.meta.env.VITE_API_URL;
 
+export const getImageUrl = (image) => {
     if (!image) {
         return "/default-profile.png";
     }
 
-    // Preview image (browser blob)
+    // Preview image
     if (image.startsWith("blob:")) {
         return image;
     }
 
-    // Already complete URL
+    // Already a full URL
     if (image.startsWith("http")) {
         return image;
     }
 
-    return `http://127.0.0.1:8000${image}`;
+    return `${BASE_URL}${image}`;
 };
