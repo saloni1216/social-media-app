dec89ed31d433d2c60a7b7f0fb6c02a3ebc839b7
Saloni singh <131551585+saloni1216@users.noreply.github.com>
2026-07-15 21:17:00 +0530

    CORS_ALLOWED_ORIGINS  update

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,9 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-]
+
 
 ROOT_URLCONF = 'config.urls'
 
