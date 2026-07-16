ed13594ae1f37dbc8dfa658c26428f44deaaf935
Saloni singh <131551585+saloni1216@users.noreply.github.com>
2026-07-16 13:07:41 +0530

    setting.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,7 +9,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
-DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
