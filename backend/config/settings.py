fa636817aa414b4ea4e0127157c14ea2625188b2
Saloni singh <131551585+saloni1216@users.noreply.github.com>
2026-07-16 15:44:55 +0530

    settings,py update

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -117,12 +117,11 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
-# Cloudinary package compatibility
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 AUTH_USER_MODEL = "accounts.CustomUser"
