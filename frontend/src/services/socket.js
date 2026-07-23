fe087f48df25d433b8032a9121755df3ff6e8760
Saloni singh <131551585+saloni1216@users.noreply.github.com>
2026-07-23 12:34:43 +0530

    update chat section

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,35 +1,101 @@
-
 let socket = null;
+let currentConversation = null;
 
 export const connectSocket = (conversationId) => {
 
-  if (socket) {
-    socket.close();
+  // Already connected to same conversation
+  if (
+      socket &&
+      currentConversation === conversationId &&
+      socket.readyState === WebSocket.OPEN
+  ) {
+      return socket;
   }
+
+  // Close only if changing conversation
+  if (
+      socket &&
+      currentConversation !== conversationId
+  ) {
+      socket.close();
+  }
+
+  currentConversation = conversationId;
 
   const token = localStorage.getItem("access_token");
 
   const WS_BASE =
-    import.meta.env.VITE_WS_URL ||
-    "ws://127.0.0.1:8000/ws/chat/";
+      import.meta.env.VITE_WS_URL ||
+      "ws://127.0.0.1:8000/ws/chat/";
 
   socket = new WebSocket(
-    `${WS_BASE}${conversationId}/?token=${token}`
+      `${WS_BASE}${conversationId}/?token=${token}`
   );
 
   socket.onopen = () => {
-    console.log("✅ WebSocket Connected");
+      console.log("✅ WebSocket Connected");
   };
 
   socket.onclose = (e) => {
-    console.log("❌ WebSocket Closed", e);
+      console.log("❌ WebSocket Closed", e);
   };
 
   socket.onerror = (e) => {
-    console.log("Socket Error:", e);
+      console.log("Socket Error", e);
   };
 
   return socket;
 };
 
 export const getSocket = () => socket;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let socket = null;
+
+// export const connectSocket = (conversationId) => {
+
+//   if (socket) {
+//     socket.close();
+//   }
+
+//   const token = localStorage.getItem("access_token");
+
+//   const WS_BASE =
+//     import.meta.env.VITE_WS_URL ||
+//     "ws://127.0.0.1:8000/ws/chat/";
+
+//   socket = new WebSocket(
+//     `${WS_BASE}${conversationId}/?token=${token}`
+//   );
+
+//   socket.onopen = () => {
+//     console.log("✅ WebSocket Connected");
+//   };
+
+//   socket.onclose = (e) => {
+//     console.log("❌ WebSocket Closed", e);
+//   };
+
+//   socket.onerror = (e) => {
+//     console.log("Socket Error:", e);
+//   };
+
+//   return socket;
+// };
+
+// export const getSocket = () => socket;
