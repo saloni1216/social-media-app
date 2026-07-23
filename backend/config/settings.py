1586bab732f0071b656e694070ffdb408d89bf07
Saloni singh <131551585+saloni1216@users.noreply.github.com>
2026-07-23 13:13:08 +0530

    update chat section

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -8,7 +8,7 @@ import cloudinary
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
-DEBUG = config("DEBUG", default=True, cast=bool)
+DEBUG = config("DEBUG", default=False, cast=bool)
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
